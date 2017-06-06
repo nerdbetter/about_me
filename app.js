466c47f3a4b77f7e4d7579b7704631df00eada6b
nerdbetter <jasontlogan@gmail.com>
2017-06-06 14:39:05 -0500

    third Question

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 console.log('loaded app.js');
 var user_name = prompt('Who do I have the pleasure of speaking with today?');
-alert('Howdy! ' + user_name + ' My name is Jason Logan, and today you will learn a few things about me from a simple Yes or No question game.');
+alert('Howdy ' + user_name + '! My name is Jason Logan, and today you will learn a few things about me from a simple Yes or No question game.');
 
 var question_one = prompt('Have I ever been Skydiving?');
 if (question_one.toLowerCase() === 'yes'){
@@ -10,7 +10,7 @@ if (question_one.toLowerCase() === 'yes'){
   alert('How did you know? I would love to get the opportunity to go again.');
 }
   else {
-    console.log('user ansered incorrectly ' + question_one + ' advised history.');
+    console.log('user ansered incorrectly ' + question_one + ', advised history.');
     alert('When I was eighteen I decided instead of a traditional graduation party I would jump out of an airplane! How exciting!');
   }
 
@@ -20,12 +20,20 @@ if (question_two.toLowerCase() === 'yes'){
   alert('You must know Wally and Joey, my two doggos! ');
 }
   else {
-    console.log('user ansered incorrectly ' + question_two + ' advised history.');
-    alert('I actually have two dogs - Wally and Joey. Wally is smart as a whip but a little needy. Joey is super goofy sometimes and has earned the nickname \'Doey\'. ')
+    console.log('user ansered incorrectly ' + question_two + ', advised history.');
+    alert('I actually have two dogs - Wally and Joey. Wally is smart as a whip but a little needy. Joey is super goofy sometimes and has earned the nickname \'Doey\'. ');
 
   }
 
 var question_three = prompt('Do I have any kids?');
+if (question_three.toLowerCase() === 'yes'){
+  console.log('user answered incorrectly ' + question_three + ', advised history.');
+  alert('I do not! Not yet anyway.');
+}
+  else {
+    console.log('user answered correctly ' + question_three );
+    alert(' You are right! Not yet anyway.');
+  }
 
 var question_four = prompt('Have I always worked in the tech industry?');
 
