12b45234377ca16837c0947355c5dded308d4ff3
Meepedy <david@psyon.org>
2017-06-09 07:15:19 -0500

    Created main function that is called in body onload

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 
 console.log('loaded app.js');
 var user_correct = 0;
+function main(){
+
+}
 var user_name = prompt('Who do I have the pleasure of speaking with today?');
 alert('Howdy ' + user_name + '! My name is Jason Logan, and today you will learn a few things about me from a simple Yes or No question game.');
 
