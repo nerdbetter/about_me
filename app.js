ad95285027f18241e34ac2d372fa58cd7098d70e
Meepedy <david@psyon.org>
2017-06-09 07:27:38 -0500

    Moved last 2 questions into functions of their own

--- app.js
+++ app.js
@@ -89,55 +89,58 @@ function question_five(){
   }
 }
 //This next part id the number guessing game
-for (var i = 0; i < 4; i++){
-  var number_guess = prompt('What is my favorite number? You get exactly 4 guesses.');
-  var favorite_number = 413;
-  if (parseInt(number_guess) === favorite_number){
-    alert('How did you know that!?');
-    console.log('user guessed number correcty');
-    user_correct++;
-    break;
-  }
-  else if (Number.isInteger(parseInt(number_guess)) === false ){
-    alert('You were supposed to guess a number!');
-    console.log('user did not guess a number');
-  }
-  else{
-    alert('That is not correct');
-    console.log('user guessed '
-    + number_guess
-    + ' ,That was incorrect');
-    if (parseInt(number_guess) < favorite_number){
-      alert('You need to guess higher!');
+function question_six() {
+  for (var i = 0; i < 4; i++){
+    var number_guess = prompt('What is my favorite number? You get exactly 4 guesses.');
+    var favorite_number = 413;
+    if (parseInt(number_guess) === favorite_number){
+      alert('How did you know that!?');
+      console.log('user guessed number correcty');
+      user_correct++;
+      break;
+    }
+    else if (Number.isInteger(parseInt(number_guess)) === false ){
+      alert('You were supposed to guess a number!');
+      console.log('user did not guess a number');
     }
     else{
-      alert('You need to guess lower!');
+      alert('That is not correct');
+      console.log('user guessed '
+      + number_guess
+      + ' ,That was incorrect');
+      if (parseInt(number_guess) < favorite_number){
+        alert('You need to guess higher!');
+      }
+      else{
+        alert('You need to guess lower!');
+      }
     }
   }
 }
-
-var lived = ['California', 'North Carolina', 'Mississippi', 'Florida' ];
-var us_states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands',
-  'Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota',
-  'Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
-i = 0;
-while ( i < 7){
-  var states_guess = prompt('I have lived in four states other than Iowa, can you guess one?');
-  if (lived.indexOf(states_guess) === 0){
-    alert('That\'s correct! I\'ve lived in ' + lived + ' .Although I moved around a bit as a child and in the service, I consider Iowa my home!');
-    user_correct++;
-    break;
-  }
-  else if (us_states.indexOf(states_guess) == -1 ){
-    alert('That is not a state!, Guess again!');
-    i++;
-  }
-  else if (i >= 6){
-    alert('Nice try, I have lived in ' + lived + ' .Although I moved around a bit as a child and in the service, I consider Iowa my home!');
-  }
-  else {
-    alert('I have not lived there, try again!');
-    i++;
+function question_seven(){
+  var lived = ['California', 'North Carolina', 'Mississippi', 'Florida' ];
+  var us_states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands',
+    'Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota',
+    'Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
+  attempts = 0;
+  while ( attempts < 7){
+    var states_guess = prompt('I have lived in four states other than Iowa, can you guess one?');
+    if (lived.indexOf(states_guess) === 0){
+      alert('That\'s correct! I\'ve lived in ' + lived + ' .Although I moved around a bit as a child and in the service, I consider Iowa my home!');
+      user_correct++;
+      break;
+    }
+    else if (us_states.indexOf(states_guess) == -1 ){
+      alert('That is not a state!, Guess again!');
+      attempts++;
+    }
+    else if (i >= 6){
+      alert('Nice try, I have lived in ' + lived + ' .Although I moved around a bit as a child and in the service, I consider Iowa my home!');
+    }
+    else {
+      alert('I have not lived there, try again!');
+      attempts++;
+    }
   }
 }
 alert('Thanks ' + user_name + ' for getting to know me a little bit. You got ' + user_correct + ' questions right!');
